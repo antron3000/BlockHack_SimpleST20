2c53e734de0a74019902189b67daec607999560b
Antoine De Vuyst <fortyonepointsix@gmail.com>
2019-04-27 09:17:20 -0400

    first commit

---
+++ contracts/ST20.sol
@@ -0,0 +1,152 @@
+
+ pragma solidity ^0.5.7;
+
+
+/**
+ * @title ERC20 interface
+ * @dev see https://eips.ethereum.org/EIPS/eip-20
+ */
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address who) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract IST20 {
+
+    // off-chain hash
+    bytes32 public tokenDetails;
+
+    //transfer, transferFrom must respect the result of verifyTransfer
+    function verifyTransfer(address _from, address _to, uint256 _amount) view public returns (bool success);
+
+    //used to create tokens
+    function mint(address _investor, uint256 _amount) public returns (bool success);
+
+    event Minted(address indexed _to, uint256 _value);
+
+}
+
+contract ST20 is IERC20,IST20 {
+
+    mapping (address => uint256) private balances;
+
+    mapping (address => mapping (address => uint256)) private allowed;
+
+    mapping(address => bool) private whitelist;
+
+    uint256 public totalSupply;
+
+    address public ContractOwner;
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    constructor() public {
+        ContractOwner = msg.sender;
+    }
+
+
+
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param owner The address to query the balance of.
+     * @return A uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address owner) public view returns (uint256) {
+        return balances[owner];
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return allowed[owner][spender];
+    }
+
+    /**
+     * @dev Transfer token to a specified address.
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function transfer(address to, uint256 value) public returns (bool) {
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowed[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(allowed[from][to] >= value);
+        require(balances[from] >= value);
+        allowed[from][to] -= value;
+        balances[to] = balances[to] += value;
+        balances[from] -= value;
+        return true;
+    }
+
+
+     function whitelistInvestor(address _investor) public {
+        whitelist[_investor] = true;
+    }
+
+        //transfer, transferFrom must respect the result of verifyTransfer
+    function verifyTransfer(address _from, address _to, uint256 _amount) view public returns (bool success){
+        if(whitelist[_from]==true && whitelist[_to]==true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //used to create tokens
+    function mint(address _investor, uint256 _amount) public returns(bool){
+        require(msg.sender==ContractOwner);
+        balances[_investor] += _amount;
+        emit Minted(_investor,_amount);
+        return true;
+    }
+
+
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,409 @@
+var provider;
+var _ethers;
+var signer;
+
+var ST20
+var ContractAddress = "0x43e426b811e9bd4ecc6efd1e1a31b7fecd1c1e68"
+var ST20ABI = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "isWhitelisted",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_investor",
+				"type": "address"
+			},
+			{
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "mint",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "ContractOwner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "verifyTransfer",
+		"outputs": [
+			{
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_investor",
+				"type": "address"
+			}
+		],
+		"name": "whitelistInvestor",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "tokenDetails",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "Minted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	}
+]
+
+async function enableEthereum() {
+	await ethereum.enable()
+}
+
+function initialize(provider){
+      provider.listAccounts().then(function(accounts) {
+				signer = provider.getSigner(accounts[0])
+        console.log(signer)
+				console.log(signer._address)
+				ST20 = new ethers.Contract(ContractAddress, ST20ABI, signer);
+        getInitialBalance();
+    })
+
+}
+
+async function getInitialBalance(){
+  let symbol = await ST20.symbol()
+  let balance = await ST20.balanceOf(signer._address);
+  document.getElementById("balanceLabelTop").innerHTML = "Your Balance: " + balance + " " + symbol;
+}
+
+async function getBalance(){
+  let address = document.getElementById("balanceOf").value;
+  let balance = await ST20.balanceOf(address);
+  document.getElementById("balanceLabel").innerHTML = balance;
+}
+
+async function isWhitelisted(){
+  let address = document.getElementById("isWhitelisted").value;
+  let whitelisted = await ST20.isWhitelisted(address);
+  document.getElementById("whitelistedLabel").innerHTML = whitelisted;
+}
+
+
+async function transfer(){
+
+	let to = document.getElementById("toAddress").value;
+  let amount = document.getElementById("transferAmount").value
+	await ST20.transfer(to,amount)
+}
+
+async function mint() {
+  let to = document.getElementById("mintToAddress").value;
+  let amount = document.getElementById("mintAmount").value;
+	await ST20.mint(to,amount)
+}
+
+async function whitelistInvestor(){
+  let investor = document.getElementById("investorAddress").value;
+  await ST20.whitelistInvestor(investor)
+}
